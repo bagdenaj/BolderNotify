be0a6d6615cd1cf6eab16b701b618cdfee7650c6
bagdenaj <76729917+bagdenaj@users.noreply.github.com>
2021-04-14 20:28:20 +0200

    Delete twiliotest.py

--- twiliotest.py
+++
@@ -1,9 +0,0 @@
-from twilio.rest import Client
-
-# Your Account Sid and Auth Token from twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = 'AC8c43dcb7c3476ff212bd92de120ae6ae'
-auth_token = 'fdfd9eb1be27efd6e51422497f49b5ff'
-client = Client(account_sid, auth_token)
-
-client.api.account.messages.create(to="+4917671227950",from_="+12542390909",body="Jetzt kannst du dich anmelden!")
